fb2e4a73dd736961958bababcb21aa2d8c3d641e
Matthew Centeri <mattcenteri@gmail.com>
2021-04-05 03:46:10 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,36 @@
+function add(a,b){
+  return a + b;
+}
+function subtract(a,b){
+  return a - b;
+}
+function multiply(a,b){
+  return a * b;
+}
+function divide(a,b){
+  return a / b;
+}
+function inc(n){
+  return n += 1;
+}
+function dec(n){
+  return n -= 1;
+}
+function makeInt(n) {
+  return parseInt(n,10 )
+}
+function preserveDecimal(n) {
+  return parseFloat(n)
+}
+var n = 956;
+function increment(){
+  n++;
+  return n;
+}
+var n = 569;
+function decrement(){
+  n--;
+  return n;
+}
+       
+  
